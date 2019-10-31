5fea1a0b81bbb9b92d20eb7d8c7943c8a648f14e
shostkotimofei@gmail.com <Timohgako1814>
2019-10-31 10:31:40 +0300

    Added Jersey config

--- src/main/java/com/company/odataapp/config/JerseyConfig.java
+++ src/main/java/com/company/odataapp/config/JerseyConfig.java
@@ -1,4 +1,58 @@
 package com.company.odataapp.config;
 
-public class JerseyConfig {
+import com.company.odataapp.odatajpaservicefactory.PeopleODataJPAServiceFactory;
+import org.apache.olingo.odata2.api.ODataServiceFactory;
+import org.apache.olingo.odata2.core.rest.ODataRootLocator;
+import org.apache.olingo.odata2.core.rest.app.ODataApplication;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Path;
+
+/**
+ * Jersey JAX-RS configuration
+ *
+ * @author Philippe
+ */
+@Component
+@ApplicationPath("/odata")
+public class JerseyConfig extends ResourceConfig {
+
+
+    public JerseyConfig(PeopleODataJPAServiceFactory serviceFactory, EntityManagerFactory emf) {
+
+        ODataApplication app = new ODataApplication();
+
+        app
+                .getClasses()
+                .forEach(c -> {
+                    // Avoid using the default RootLocator, as we want
+                    // a Spring Managed one
+                    if (!ODataRootLocator.class.isAssignableFrom(c)) {
+                        register(c);
+                    }
+                });
+
+        register(new PeopleRootLocator(serviceFactory));
+        register(new PeopleODataJPAServiceFactory.EntityManagerFilter(emf));
+    }
+
+    @Path("/")
+    public static class PeopleRootLocator extends ODataRootLocator {
+
+        private PeopleODataJPAServiceFactory serviceFactory;
+
+        public PeopleRootLocator(PeopleODataJPAServiceFactory serviceFactory) {
+            this.serviceFactory = serviceFactory;
+        }
+
+        @Override
+        public ODataServiceFactory getServiceFactory() {
+            return this.serviceFactory;
+        }
+
+    }
+
 }

ce255f0db2bb3eb8fb71712d85972f8e782c8cbc
shostkotimofei@gmail.com <Timohgako1814>
2019-10-31 10:33:50 +0300

    Added Root Locator

--- src/main/java/com/company/odataapp/rootlocator/PeopleRootLocator.java
+++ src/main/java/com/company/odataapp/rootlocator/PeopleRootLocator.java
@@ -1,4 +1,21 @@
 package com.company.odataapp.rootlocator;
 
-public class PeopleRootLocator {
+import com.company.odataapp.odatajpaservicefactory.PeopleODataJPAServiceFactory;
+import org.apache.olingo.odata2.api.ODataServiceFactory;
+import org.apache.olingo.odata2.core.rest.ODataRootLocator;
+
+import javax.ws.rs.Path;
+
+@Path("/")
+public class PeopleRootLocator extends ODataRootLocator {
+    private PeopleODataJPAServiceFactory serviceFactory;
+
+    public PeopleRootLocator(PeopleODataJPAServiceFactory serviceFactory) {
+        this.serviceFactory = serviceFactory;
+    }
+
+    @Override
+    public ODataServiceFactory getServiceFactory() {
+        return this.serviceFactory;
+    }
 }

fcc67dd1b3479624e9c84d5aae6ad319a0289445
shostkotimofei@gmail.com <Timohgako1814>
2019-11-01 17:32:13 +0300

    Added config for HANA database

--- src/main/java/com/company/odataapp/config/CloudDatabaseConfig.java
+++ src/main/java/com/company/odataapp/config/CloudDatabaseConfig.java
@@ -1,4 +1,24 @@
 package com.company.odataapp.config;
 
-public class CloudDatabaseConfig {
+import org.springframework.cloud.config.java.AbstractCloudConfig;
+
+//@Configuration
+//@Profile("cloud")
+public class CloudDatabaseConfig extends AbstractCloudConfig {
+//
+//    @Bean
+//    public DataSource dataSource(@Value("${hana.url}") final String url,
+//                                 @Value("${hana.user}") final String user,
+//                                 @Value("${hana.password}") final String password) {
+//
+//
+//        return DataSourceBuilder.create()
+//                .type(HikariDataSource.class)
+//                .driverClassName(com.sap.cloud)
+//                .url(url)
+//                .username(user)
+//                .password(password)
+//                .build();
+//
+//    }
 }
